427356d321154de001c2891a030210978775297e
bezmuth <benkel97@protonmail.com>
2023-04-01 01:38:10 +0100

    Stuff

--- src/db.rs
+++ src/db.rs
@@ -1,73 +1,76 @@
-
-
 use bincode::{Decode, Encode};
 use rand::{distributions::Alphanumeric, Rng};
 
 #[derive(Encode, Decode, PartialEq, Debug)]
-struct userValue {
-    phash : String,
-    feeds : Vec<String>,
-    token : String
+struct UserValue {
+    phash: String,
+    feeds: Vec<String>,
+    token: String,
 }
 
-
 pub fn init() -> sled::Db {
-    let exampleData = userValue {
-        feeds : vec!["https://xeiaso.net/blog.rss".to_string(), "https://100r.co/links/rss.xml".to_string(), "https://asahilinux.org/blog/index.xml".to_string()],
-        phash : "deadbeef".to_string(),
-        token : "1".to_string(),
+    let example_data = UserValue {
+        feeds: vec![
+            "https://xeiaso.net/blog.rss".to_string(),
+            "https://100r.co/links/rss.xml".to_string(),
+            "https://asahilinux.org/blog/index.xml".to_string(),
+        ],
+        phash: "deadbeef".to_string(),
+        token: "1".to_string(),
     };
-    let bins = bincode::encode_to_vec(exampleData, bincode::config::standard()).unwrap();
+    let bins = bincode::encode_to_vec(example_data, bincode::config::standard()).unwrap();
     let db: sled::Db = sled::open("db.sled").unwrap();
-    db.insert(b"reocha", bins);
+    db.insert(b"reocha", bins).unwrap();
     db
 }
 
-fn decodeUserValue(userBytes: sled::IVec) -> userValue {
-    bincode::decode_from_slice(&userBytes[..], bincode::config::standard()).unwrap().0
+fn decode_user_value(user_bytes: sled::IVec) -> UserValue {
+    bincode::decode_from_slice(&user_bytes[..], bincode::config::standard())
+        .unwrap()
+        .0
 }
 
-pub fn getUsersFeeds(user : &str, db : &sled::Db) -> Vec<String> {
-    decodeUserValue(db.get(user).unwrap().unwrap()).feeds
+pub fn get_users_feeds(user: &str, db: &sled::Db) -> Vec<String> {
+    decode_user_value(db.get(user).unwrap().unwrap()).feeds
 }
 
-pub fn update_user_feeds(user : &str, db : &sled::Db, feeds : Vec<String>) {
-    let mut newData = decodeUserValue(db.get(user).unwrap().unwrap());
-    newData.feeds = feeds;
-    let bins = bincode::encode_to_vec(newData, bincode::config::standard()).unwrap();
-    db.remove(user.as_bytes());
-    db.insert(user.as_bytes(), bins);
+pub fn update_user_feeds(user: &str, db: &sled::Db, feeds: Vec<String>) {
+    let mut new_data = decode_user_value(db.get(user).unwrap().unwrap());
+    new_data.feeds = feeds;
+    let bins = bincode::encode_to_vec(new_data, bincode::config::standard()).unwrap();
+    db.remove(user.as_bytes()).unwrap();
+    db.insert(user.as_bytes(), bins).unwrap();
 }
 
-fn update_user_token(user : &str, db : &sled::Db, token : String){
-    let mut newData = decodeUserValue(db.get(user).unwrap().unwrap());
-    newData.token = token;
-    let bins = bincode::encode_to_vec(newData, bincode::config::standard()).unwrap();
-    db.remove(user.as_bytes());
-    db.insert(user.as_bytes(), bins);
+fn update_user_token(user: &str, db: &sled::Db, token: String) {
+    let mut new_data = decode_user_value(db.get(user).unwrap().unwrap());
+    new_data.token = token;
+    let bins = bincode::encode_to_vec(new_data, bincode::config::standard()).unwrap();
+    db.remove(user.as_bytes()).unwrap();
+    db.insert(user.as_bytes(), bins).unwrap();
 }
 
-pub fn tryLogin(username : &str, password : &str, db : &sled::Db) -> Option<String> {
+pub fn try_login(username: &str, password: &str, db: &sled::Db) -> Option<String> {
     if let Ok(data) = db.get(username) {
-        let decoded = decodeUserValue(data.unwrap());
-        if decoded.phash == password{
-            let token : String = rand::thread_rng()
+        let decoded = decode_user_value(data.unwrap());
+        if decoded.phash == password {
+            let token: String = rand::thread_rng()
                 .sample_iter(&Alphanumeric)
                 .take(64)
                 .map(char::from)
                 .collect();
             update_user_token(username, db, token.clone());
-            return Some(token)
+            return Some(token);
         }
     }
     None
 }
 
-pub fn check_token(username : &str, token: &str, db : &sled::Db) -> bool{
+pub fn check_token(username: &str, token: &str, db: &sled::Db) -> bool {
     if let Ok(data) = db.get(username) {
-        let decoded = decodeUserValue(data.unwrap());
-        if decoded.token == token{
-            return true
+        let decoded = decode_user_value(data.unwrap());
+        if decoded.token == token {
+            return true;
         }
     }
     false
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
-#[macro_use] extern crate rocket;
-use rocket::{State};
-use rocket::response::{Flash, Redirect};
-use rocket::form::Form;
-use rocket_dyn_templates::{Template, context};
-use rocket::http::{Cookie, CookieJar};
+#[macro_use]
+extern crate rocket;
 use chrono::prelude::*;
+use rocket::form::Form;
+use rocket::http::{Cookie, CookieJar};
+use rocket::response::{Flash, Redirect};
+use rocket::State;
+use rocket_dyn_templates::{context, Template};
 
 use std::cmp::Ordering;
-use std::collections::{HashMap};
-use url::Url;
+use std::collections::HashMap;
 use std::sync::Mutex;
+use url::Url;
 
 mod db;
 mod feed;
 
-struct Users (sled::Db);
-struct FeedCache (Mutex<HashMap<String, Vec<FeedItem>>>);
-
+struct Users(sled::Db);
+struct FeedCache(Mutex<HashMap<String, Vec<FeedItem>>>);
 
 #[derive(Eq, PartialEq, serde::Serialize, Clone)]
 struct FeedItem {
-    date :  i64,
-    title : String,
-    link : String,
-    site : String,
-    stale_time : i64,
+    date: i64,
+    title: String,
+    link: String,
+    site: String,
+    stale_time: i64,
 }
 
 impl Ord for FeedItem {
@@ -40,32 +40,36 @@ impl PartialOrd for FeedItem {
 }
 
 // parses a url and returns the domain
-fn parse_domain(url: String) -> String{
+fn parse_domain(url: String) -> String {
     let domain = Url::parse(&url);
     match domain {
         Ok(v) => v.host_str().unwrap().to_string(),
-        Err(_) => url
+        Err(_) => url,
     }
 }
 
-fn check_cookie(cookies: &CookieJar<'_>, users: &sled::Db, username : &str) -> bool{
-    if let Some(token) = cookies.get("token"){
+fn check_cookie(cookies: &CookieJar<'_>, users: &sled::Db, username: &str) -> bool {
+    if let Some(token) = cookies.get("token") {
         println!("{}", token.value());
-        if db::check_token(username, token.value(), users){
-            return true
+        if db::check_token(username, token.value(), users) {
+            return true;
         }
     }
     false
 }
 
 #[get("/login")]
-fn login(_users : &State<Users>) -> Template {
+fn login(_users: &State<Users>) -> Template {
     Template::render("login", context! {})
 }
 
 #[post("/login", data = "<login_data>")]
-fn login_post(cookies: &CookieJar<'_>, users : &State<Users>, login_data : Form<Vec<String>>) -> Flash<Redirect> {
-    if let Some(token) = db::tryLogin(&login_data.clone()[0], &login_data[1], &users.0){
+fn login_post(
+    cookies: &CookieJar<'_>,
+    users: &State<Users>,
+    login_data: Form<Vec<String>>,
+) -> Flash<Redirect> {
+    if let Some(token) = db::try_login(&login_data.clone()[0], &login_data[1], &users.0) {
         cookies.add(Cookie::new("token", token));
         cookies.add(Cookie::new("user", login_data[0].clone()));
         Flash::success(Redirect::to(format!("/{}", login_data[0])), "Login")
@@ -74,11 +78,15 @@ fn login_post(cookies: &CookieJar<'_>, users : &State<Users>, login_data : Form<
     }
 }
 
-
 #[post("/<user>/submit", data = "<feeds>")]
-fn feedsUpdate(cookies: &CookieJar<'_>, user : &str, users : &State<Users>, feeds : Form<String>) -> Flash<Redirect> {
-    if check_cookie(cookies, &users.0, user){
-        let _feed_list = db::getUsersFeeds(user, &users.0);
+fn feeds_update(
+    cookies: &CookieJar<'_>,
+    user: &str,
+    users: &State<Users>,
+    feeds: Form<String>,
+) -> Flash<Redirect> {
+    if check_cookie(cookies, &users.0, user) {
+        let _feed_list = db::get_users_feeds(user, &users.0);
         let feeds = feeds
             .into_inner()
             .lines()
@@ -90,28 +98,36 @@ fn feedsUpdate(cookies: &CookieJar<'_>, user : &str, users : &State<Users>, feed
     Flash::error(Redirect::to("/".to_string()), "Error")
 }
 
-
 #[get("/<user>/settings")]
-fn settingsPage(cookies: &CookieJar<'_>, user : &str, users : &State<Users>) -> Template {
-    if check_cookie(cookies, &users.0, user){
-        let feed_list = db::getUsersFeeds(user, &users.0);
-        return Template::render("settings", context! {
-            feeds : feed_list,
-            username : user,
-            logged_in : true,
-        });
+fn settings_page(cookies: &CookieJar<'_>, user: &str, users: &State<Users>) -> Template {
+    if check_cookie(cookies, &users.0, user) {
+        let feed_list = db::get_users_feeds(user, &users.0);
+        return Template::render(
+            "settings",
+            context! {
+                feeds : feed_list,
+                username : user,
+                logged_in : true,
+            },
+        );
     }
-    Template::render("error", context!{})
+    Template::render("error", context! {})
 }
 
-
 #[get("/<user>")]
-fn userPage(cookies: &CookieJar<'_>, user : &str, users : &State<Users>, feed_cache : &State<FeedCache>) -> Template {
+fn user_page(
+    cookies: &CookieJar<'_>,
+    user: &str,
+    users: &State<Users>,
+    feed_cache: &State<FeedCache>,
+) -> Template {
     let _logged_in = false;
-    let feed_list = db::getUsersFeeds(user, &users.0);
-    let mut feed_items : Vec<FeedItem> = vec![];
-    for addr in feed_list{
-        if let std::collections::hash_map::Entry::Vacant(e) = feed_cache.0.lock().unwrap().entry(addr.clone()) {
+    let feed_list = db::get_users_feeds(user, &users.0);
+    let mut feed_items: Vec<FeedItem> = vec![];
+    for addr in feed_list {
+        if let std::collections::hash_map::Entry::Vacant(e) =
+            feed_cache.0.lock().unwrap().entry(addr.clone())
+        {
             let mut curr_items = vec![];
             let feed_data: String = ureq::get(&addr)
                 .set("Example-Header", "header value")
@@ -120,13 +136,13 @@ fn userPage(cookies: &CookieJar<'_>, user : &str, users : &State<Users>, feed_ca
                 .into_string()
                 .unwrap();
             let parsed_feed = feed::parse_feed_data(&feed_data);
-            for entry in parsed_feed.entries{
+            for entry in parsed_feed.entries {
                 let item = FeedItem {
-                    title : entry.title.unwrap().content,
-                    link : entry.links[0].href.clone(),
-                    date : entry.published.unwrap_or_else(Utc::now).timestamp(),
-                    site : parse_domain(addr.clone()),
-                    stale_time : Utc::now().timestamp()
+                    title: entry.title.unwrap().content,
+                    link: entry.links[0].href.clone(),
+                    date: entry.published.unwrap_or_else(Utc::now).timestamp(),
+                    site: parse_domain(addr.clone()),
+                    stale_time: Utc::now().timestamp(),
                 };
                 feed_items.push(item.clone());
                 curr_items.push(item.clone());
@@ -135,7 +151,7 @@ fn userPage(cookies: &CookieJar<'_>, user : &str, users : &State<Users>, feed_ca
         } else {
             let items = &mut feed_cache.0.lock().unwrap().get(&addr).unwrap().to_vec();
             // if the last item in the vector (i.e. the most recent) is stale, reload the feed
-            if items.last().unwrap().stale_time+600 > Utc::now().timestamp(){
+            if items.last().unwrap().stale_time + 600 > Utc::now().timestamp() {
                 items.last_mut().unwrap().stale_time = Utc::now().timestamp(); // gotta do this so we dont infinitley reload the feed if nothing new apears
                 let feed_data: String = ureq::get(&addr)
                     .set("Example-Header", "header value")
@@ -144,31 +160,34 @@ fn userPage(cookies: &CookieJar<'_>, user : &str, users : &State<Users>, feed_ca
                     .into_string()
                     .unwrap();
                 let parsed_feed = feed::parse_feed_data(&feed_data);
-                let items_as_links : Vec<String> = items.iter_mut().map(|x| x.link.clone()).collect();
-                for entry in parsed_feed.entries{
-                    if !(items_as_links.contains(&entry.links[0].href.clone())){
+                let items_as_links: Vec<String> =
+                    items.iter_mut().map(|x| x.link.clone()).collect();
+                for entry in parsed_feed.entries {
+                    if !(items_as_links.contains(&entry.links[0].href.clone())) {
                         items.push(FeedItem {
-                            title : entry.title.unwrap().content,
-                            link : entry.links[0].href.clone(),
-                            date : entry.published.unwrap_or_else(Utc::now).timestamp(),
-                            site : parse_domain(addr.clone()),
-                            stale_time : Utc::now().timestamp()
+                            title: entry.title.unwrap().content,
+                            link: entry.links[0].href.clone(),
+                            date: entry.published.unwrap_or_else(Utc::now).timestamp(),
+                            site: parse_domain(addr.clone()),
+                            stale_time: Utc::now().timestamp(),
                         });
                     }
                 }
             }
             feed_items.append(items);
         }
-
     }
     feed_items.sort();
     feed_items.reverse();
 
-    Template::render("index", context! {
-        username: user,
-        items: feed_items,
-        logged_in: check_cookie(cookies, &users.0, user),
-    })
+    Template::render(
+        "index",
+        context! {
+            username: user,
+            items: feed_items,
+            logged_in: check_cookie(cookies, &users.0, user),
+        },
+    )
 }
 
 #[launch]
@@ -176,8 +195,12 @@ pub fn rocket() -> _ {
     let users = Users(db::init());
     let feed_cache = FeedCache(Mutex::new(HashMap::new()));
 
-    rocket::build().mount("/", routes![userPage, settingsPage, feedsUpdate, login, login_post])
-                   .attach(Template::fairing())
-                   .manage(users)
-                   .manage(feed_cache)
+    rocket::build()
+        .mount(
+            "/",
+            routes![user_page, settings_page, feeds_update, login, login_post],
+        )
+        .attach(Template::fairing())
+        .manage(users)
+        .manage(feed_cache)
 }
